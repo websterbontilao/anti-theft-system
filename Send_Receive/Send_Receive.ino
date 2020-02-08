80d663d85556e453c393fd5411a5e39b9ab2cd2f
websterbontilao <websterbontilao@gmail.com>
2020-02-08 16:42:13 +0800

    Added complete logic for disable/enable

--- Send_Receive/Send_Receive.ino
+++ Send_Receive/Send_Receive.ino
@@ -1,22 +1,50 @@
 #include <SoftwareSerial.h>
+#include <TinyGPS.h>
 
 //Create software serial object to communicate with SIM900
 SoftwareSerial mySerial(10, 11);
+TinyGPS gps;
 
 String receivedData;
-String disableCode = "disable";
+String disableCode = "disabl";
 String enableCode = "enable";
 String getLocationCode = "locati";
 
+float lat, lon;
+
+bool sysEnabled;
+
 void setup() {
   // put your setup code here, to run once:
+  
+  // State Modes
+  // TRUE - Enabled
+  // FALSE - Disabled
+  sysEnabled = true;
 
+  // Set Pin for Relay Module
+  pinMode(17, OUTPUT);
+  digitalWrite(17, LOW);
+  
   //Begin serial communication with Arduino and SIM900
   mySerial.begin(9600);
   Serial.begin(9600);
+  Serial1.begin(9600);
 
   Serial.println("Initializing...");
   delay(1000);
+
+  mySerial.println("AT+IPR?"); //Handshaking with SIM900
+  updateSerial();
+
+  mySerial.println("AT+CSQ"); // SIGNAL QUALITY
+  updateSerial();
+
+  mySerial.println("AT+CCID");
+  updateSerial();
+
+  mySerial.println("AT+CCREG=?");
+  updateSerial();
 }
 
 void sendMessage(String number) {
@@ -25,12 +53,11 @@ void sendMessage(String number) {
 
    mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
   updateSerial();
-  mySerial.println("AT+CMGS=\""+number+"\"");//change ZZ with country code and xxxxxxxxxxx with phone number to sms
+  mySerial.println("AT+CMGS=\""+number+"\"");
   updateSerial();
-  mySerial.print(10.36245,6);
-//  smsSerial.print(lat,6);
+  mySerial.print(lat,7);
   mySerial.print(",");
-  mySerial.print(123.9312,6);
+  mySerial.print(lon,7);
   updateSerial();
   mySerial.write(26);
 }
@@ -40,27 +67,47 @@ void loop() {
   //  Receive message
   if (mySerial.available() > 0) {
     String messageReceived = mySerial.readString();
+    
     // code number of characters (6)
     String senderNum = messageReceived.substring(9, 22);
     receivedData = messageReceived.substring(51, 57);
     
-    if (receivedData.equals(disableCode)) {
-      Serial.println("Disabled");
-      // Do disabled code here
+    // Check if recieved code is Disabled code
+    // And check if system state is Enabled
+    if (receivedData.equals(disableCode) && sysEnabled == true) {
+      digitalWrite(17, HIGH);
+      getCurrentLocation();
+      sendMessage(senderNum);
+      toggleSystemState();
     }
 
-    if (receivedData.equals(enableCode)) {
-      Serial.println("Enabled");
-      // Do enable code here
+    if (receivedData.equals(enableCode) && sysEnabled == false) {
+      digitalWrite(17, LOW);
+      toggleSystemState();
     }
 
     if (receivedData.equals(getLocationCode)) {
       Serial.println("Get Location");
+      getCurrentLocation();
       sendMessage(senderNum);
-      // Do get location code here
     }
     
     delay(500);
+  }
+}
+
+void getCurrentLocation() {
+  bool _hasResult = false;
+  // Set serial baud rate
+
+  while(_hasResult == false){
+    while(Serial1.available() && _hasResult == false) {
+      if(gps.encode(Serial1.read()))
+      {
+        gps.f_get_position(&lat,&lon);
+        _hasResult = true;
+      }
+    }
   }
 }
 
@@ -74,5 +121,14 @@ void updateSerial()
   while(mySerial.available()) 
   {
     Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
+  }
+}
+
+void toggleSystemState() {
+  if (sysEnabled) {
+    sysEnabled = false;
+  }
+  else {
+    sysEnabled = true;
   }
 }
